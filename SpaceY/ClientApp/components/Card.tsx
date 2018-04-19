0650e0c9ee126601c42e3625f140966071694747
hackehackspett <hackehackspett1337@hotmail.com>
2018-04-19 15:40:45 +0200

    Page for testing drag and drop implementation

---
+++ SpaceY/ClientApp/components/Card.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { RouteComponentProps } from "react-router";
+
+
+
+/**
+ * Test of drag and drop
+ */
+export class Card extends React.Component<RouteComponentProps<any>> {
+    constructor(props: RouteComponentProps<any>) {
+		super(props)
+		//this.moveCard = this.moveCard.bind(this)
+		this.state = {
+			cards: [
+				{
+					id: 1,
+					text: 'Write a cool JS library',
+				},
+				{
+					id: 2,
+					text: 'Make it generic enough',
+				},
+				{
+					id: 3,
+					text: 'Write README',
+				},
+				{
+					id: 4,
+					text: 'Create some examples',
+				},
+				{
+					id: 5,
+					text:
+						'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
+				},
+				{
+					id: 6,
+					text: '???',
+				},
+				{
+					id: 7,
+					text: 'PROFIT',
+				},
+			],
+		}
+	}
+
+    render() {
+        return <div>
+                  
+               </div>;
+    }
+    
+    
+    
+
+    
+    
+}
