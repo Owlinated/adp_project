0aad6976c623d118d0ad7c411c4ad738aa4c03ad
Unknown <florian@stellbr.ink>
2018-05-18 15:10:20 +0200

    #46 Fix spacing when formatting

--- SpaceY/ClientApp/support/EquationFormatter.tsx
+++ SpaceY/ClientApp/support/EquationFormatter.tsx
@@ -1,12 +1,19 @@
 import { IRestEquation } from "../interface/IRestEquation";
 
 export function formatEquation(equation: IRestEquation, references: IRestEquation[]): string {
+    let result = equation.equation;
+
     // Substitute default operations
-    let result = equation.equation.replace(/\//g, "÷")
+    result = result
+        .replace(/\//g, "÷")
         .replace(/\*/gi, "×")
         .replace(/\//gi, "÷")
         .replace(/Sin\(/gi, "sin(")
         .replace(/Cos\(/gi, "cos(");
+
+    // Fix spacing around operators
+    result = result
+        .replace(/\s*([×÷\+\-])\s*/g, " $1 ");
 
     // Substitute used variables
     result = equation.parameters.reduce((acc, parameter, index) => {
