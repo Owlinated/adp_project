8851bf256531c6d1fa829779964ec4f585c7325d
Unknown <florian@stellbr.ink>
2018-05-18 14:11:31 +0200

    #46 Add equation formatting

--- SpaceY/ClientApp/components/Equation.tsx
+++ SpaceY/ClientApp/components/Equation.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { IRestEquation } from "../interface/IRestEquation";
 import { IRestEquationParam } from "../interface/IRestEquationParam";
 import { IRestNestedEquation } from "../interface/IRestNestedEquation";
+import { formatEquation } from "../support/EquationFormatter";
 
 interface IEquationState {
     result: number | undefined;
@@ -69,7 +70,7 @@ export class Equation extends React.Component<IRestNestedEquation, IEquationStat
                     <div className="panel-heading">
                         <h4 className="panel-title">
                             <a data-toggle="collapse" href={`#collapse${reference.id}`}>
-                                {reference.equation}
+                                {reference.description}
                             </a>
                         </h4>
                     </div>
@@ -88,9 +89,10 @@ export class Equation extends React.Component<IRestNestedEquation, IEquationStat
      * @param equation The equation to render
      */
     public renderEquation(equation: IRestEquation, isMain: boolean) {
+        const formattedEquation = formatEquation(equation, this.props.references);
         return (
             <div>
-                <p>{isMain ? `${equation.equation} = ${this.state.result}` : equation.equation}</p>
+                <p>{isMain ? `${formattedEquation} = ${this.state.result}` : formattedEquation}</p>
                 {equation.parameters.map((parameter, index) => this.renderParameter(equation, parameter, index))}
             </div>
         );
--- SpaceY/ClientApp/support/EquationFormatter.tsx
+++ SpaceY/ClientApp/support/EquationFormatter.tsx
@@ -1,16 +1,27 @@
-﻿import {IRestNestedEquation} from "../interface/IRestNestedEquation"
+import { IRestEquation } from "../interface/IRestEquation";
 
-export function FormatEquation(equation: IRestNestedEquation): string {
-    const result = equa.replace(/\//g, "÷")
+export function formatEquation(equation: IRestEquation, references: IRestEquation[]): string {
+    // Substitute default operations
+    let result = equation.equation.replace(/\//g, "÷")
         .replace(/\*/g, "×")
-        .replace(/v/g, "V")
-        .replace(/Var\(0\)/g, "X")
-        .replace(/Var\(1\)/g, "Y")
-        .replace(/Var\(2\)/g, "Z")
-        .replace(/Sin\(/g, "s")
-        .replace(/Cos\(/g, "c")
-        .replace(/Ref\(/g, "e")
-        .replace(/ref\(/g, "e")
-        .replace(/\s/g, "")
-    return "asdf";
+        .replace(/\//g, "÷")
+        .replace(/Sin\(/g, "sin(")
+        .replace(/Cos\(/g, "cos(");
+
+    // Substitute used variables
+    result = equation.parameters.reduce((acc, parameter, index) => {
+            const regex = new RegExp(`Var\\(${index}\\)`, "g");
+            return acc.replace(regex, parameter.name);
+        },
+        result);
+
+    // Substitute referenced equations
+    result = references.reduce((acc, reference) => {
+            const regex = new RegExp(`Ref\\(${reference.id}\\)`, "g");
+            const parameters = reference.parameters.map((parameter) => parameter.name);
+            return acc.replace(regex, `${reference.description}(${parameters.join(", ")})`);
+        },
+        result);
+
+    return result;
 }
